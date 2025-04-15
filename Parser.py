acaffa803a377eba57b2d26e2976607e27e40729
m_luc <m_luchin@mail.ru>
2025-04-15 15:01:01 +0400

    Часть финальной лабы

--- Parser.py
+++ Parser.py
@@ -3,37 +3,49 @@ from PIL import  Image, ImageOps
 import baricentric as bc
 import normals as norm
 
-#Моделька
-f = open("model_1.obj")
+model = open("12268_banjofrog_v1_L3.obj")
+IMGTexture_exist = False
+VTexture_exist = False
+texture_img = None
+try:
+    texture_img = Image.open("bunny-atlas.jpg")
+    IMGTexture_exist = True  # Успешно загружено
+except Exception as e:
+    print(f"Ошибка загрузки изображения: {e}")
+    IMGTexture_exist = False
 
-texture = np.array(ImageOps.flip(Image.open("bunny-atlas.jpg")))
+if IMGTexture_exist:
+    texture = np.array(ImageOps.flip(texture_img))
 
 vectorv = []
 vectorf = []
 texture_coords = []
 texture_nums = []
-for line in f:
+for line in model:
     v = line.split()
-
+    if len(v) == 0:
+        continue
     if (v[0] == "vt"):
         texture_coords.append([float(v[1]), float(v[2])])
+        VTexture_exist = True
     if (v[0] == "v"):
         vectorv.append([float(v[1]), float(v[2]), float(v[3])])
     if (v[0] == "f"):
-        v1 = v[1].split('/')[0]
-        v2 = v[2].split('/')[0]
-        v3 = v[3].split('/')[0]
-        th1 = v[1].split('/')[1]
-        th2 = v[2].split('/')[1]
-        th3 = v[3].split('/')[1]
-        vectorf.append([int(v1), int(v2), int(v3)])
-        texture_nums.append([int(th1), int(th2), int(th3)])
+        for i in range(2, len(v) - 1):
+            v1 = v[1].split('/')[0]
+            v2 = v[i].split('/')[0]
+            v3 = v[i+1].split('/')[0]
+            th1 = v[1].split('/')[1]
+            th2 = v[i].split('/')[1]
+            th3 = v[i+1].split('/')[1]
+            vectorf.append([int(v1), int(v2), int(v3)])
+            texture_nums.append([int(th1), int(th2), int(th3)])
 
 #Соединить все вершины треугольника, нарисовать хранится в виде: (x1, x2, x3)(y1,y2,y3)(z1,z2,z3)
 
 img_mat2 = np.zeros(shape=(bc.high, bc.widh, 3), dtype=np.uint8)
 z_buff_mat = np.full((bc.high, bc.widh), np.inf, dtype=np.float64)
-vectorv = norm.resize(vectorv,0,-0.05,1*bc.coef,np.radians(0),np.radians(0),np.radians(0))
+vectorv = norm.resize(vectorv,0.1, -0.1, 60*bc.coef,np.radians(150),np.radians(0),np.radians(210))
 normalsArr = np.zeros((len(vectorv), 3))
 normalsArr = norm.get_point_normals(vectorv, vectorf, normalsArr)
 
@@ -53,8 +65,10 @@ for i in range(0,len(vectorf)):
     light = norm.cut_nofacial(x0, y0, z0, x1, y1, z1, x2, y2, z2)
     color = (-255 * light,-255*light,-255*light)
     if (norm.cut_nofacial(x0, y0, z0, x1, y1, z1, x2, y2, z2) < 0):
-        bc.draw_tr(img_mat2, z_buff_mat, color, polygon_texture_nums, texture_coords, texture, x0, y0, z0, x1, y1, z1, x2, y2, z2, normalsArr[vectorf[i][0] - 1], normalsArr[vectorf[i][1] - 1], normalsArr[vectorf[i][2] - 1])
-
+        if IMGTexture_exist and VTexture_exist:
+            bc.draw_tr_textured(img_mat2, z_buff_mat, polygon_texture_nums, texture_coords, texture, x0, y0, z0, x1, y1, z1, x2, y2, z2, normalsArr[vectorf[i][0] - 1], normalsArr[vectorf[i][1] - 1], normalsArr[vectorf[i][2] - 1])
+        else:
+            bc.draw_tr(img_mat2, z_buff_mat, x0, y0, z0, x1, y1, z1, x2, y2, z2, normalsArr[vectorf[i][0] - 1], normalsArr[vectorf[i][1] - 1], normalsArr[vectorf[i][2] - 1])
 
 img = Image.fromarray(img_mat2, mode="RGB")
 img = ImageOps.flip(img)
--- baricentric.py
+++ baricentric.py
@@ -10,7 +10,7 @@ def bari(x0, y0, x1, y1, x2, y2, x, y):
     lambda2 = 1.0 - lambda0 - lambda1
     return lambda0, lambda1, lambda2
 
-def draw_tr(img_mat, z_buff, color, polygon_texture_nums, texture_coords, texture, x0, y0, z0, x1, y1, z1, x2, y2, z2,  i0, i1, i2):
+def draw_tr_textured(img_mat, z_buff, polygon_texture_nums, texture_coords, texture, x0, y0, z0, x1, y1, z1, x2, y2, z2, i0, i1, i2):
     a = 10000 * coef
     px0, py0 = a * x0 / z0 + widh / 2, a * y0 / z0 + high / 2
     px1, py1 = a * x1 / z1 + widh / 2, a * y1 / z1 + high / 2
@@ -52,4 +52,41 @@ def draw_tr(img_mat, z_buff, color, polygon_texture_nums, texture_coords, textur
 
                     color = texture[int(Wid*(l0*texture_coords[UVt1-1][1] +  l1*texture_coords[UVt2-1][1] + l2*texture_coords[UVt3-1][1]))][int(High*(l0*texture_coords[UVt1-1][0] +  l1*texture_coords[UVt2-1][0] + l2*texture_coords[UVt3-1][0]))]
                     img_mat[y][x] = color * -Intence
+                    z_buff[y][x] = z_cord
+
+def draw_tr(img_mat, z_buff, x0, y0, z0, x1, y1, z1, x2, y2, z2, i0, i1, i2):
+    a = 10000 * coef
+    px0, py0 = a * x0 / z0 + widh / 2, a * y0 / z0 + high / 2
+    px1, py1 = a * x1 / z1 + widh / 2, a * y1 / z1 + high / 2
+    px2, py2 = a * x2 / z2 + widh / 2, a * y2 / z2 + high / 2
+
+    I0 = i0[2]
+    I1 = i1[2]
+    I2 = i2[2]
+
+    xmin = min(px0, px1, px2)
+    xmax = max(px0, px1, px2)
+    ymin = min(py0, py1, py2)
+    ymax = max(py0, py1, py2)
+    if (xmin < 0): xmin = 0
+    if (ymin < 0): ymin = 0
+    if (xmax > widh): xmax = widh
+    if (ymax > high): ymax = high
+
+    xmax = math.ceil(xmax)
+    ymax = math.ceil(ymax)
+    xmin = math.floor(xmin)
+    ymin = math.floor(ymin)
+
+    for x in range (xmin, xmax):
+        for y in range (ymin, ymax):
+            l0, l1, l2 = bari(px0,py0,px1,py1,px2,py2, x, y)
+            if (l0 >= 0 and l1>= 0 and l2 >= 0):
+                z_cord = l0*z0 + l1*z1 + l2*z2
+                if z_cord > z_buff[y][x]:
+                    continue
+                else:
+                    color = l0 * I0 + l1 * I1 + l2 * I2
+                    color *= -255
+                    img_mat[y][x] = color
                     z_buff[y][x] = z_cord
